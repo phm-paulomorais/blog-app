ec59baf7472dd3ebab4b8241fba1985482c3dcb4
Paulo Morais <phm.paulomorais@gmail.com>
2022-07-31 16:59:17 -0300

    Ruby on Rails 3.2 Blog in 15 minutes Step by Step

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,7 @@
+class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create!(params[:comment].permit!)
+        redirect_to @post
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+	
+	has_many :comments
+
+	validates_presence_of :body, :title
+end
